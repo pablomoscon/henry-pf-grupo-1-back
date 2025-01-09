577e2ef87c1940262490f42a8f3b8c0dd6f72a6f
pablomos <mosconpablo@gmail.com>
2025-01-09 11:59:59 -0300

    feat/create-rooms-dtos

--- src/modules/rooms/entities/room.entity.ts
+++ src/modules/rooms/entities/room.entity.ts
@@ -1,1 +1,28 @@
-export class Room {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNumber, IsBoolean, IsOptional, IsArray } from 'class-validator';
+
+export class CreateRoomDto {
+    @ApiProperty({ description: 'Name of the room', example: 'Deluxe Suite' })
+    @IsString()
+    name: string;
+
+    @ApiProperty({
+        description: 'Images URLs for the room',
+        example: ['https://example.com/product-image.jpg', 'https://example.com/product-image2.jpg'],
+    })
+    @IsArray()
+    @IsString({ each: true })
+    imgs: string[];
+
+    @ApiProperty({ description: 'Description of the room', example: 'A luxurious suite with a sea view.' })
+    @IsString()
+    description: string;
+
+    @ApiProperty({ description: 'Price of the room', example: 150.00 })
+    @IsNumber()
+    price: number;
+
+    @ApiProperty({ description: 'Availability of the room', example: true })
+    @IsBoolean()
+    available: boolean;
+}
