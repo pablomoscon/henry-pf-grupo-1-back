ec5851be3429ad9ce0ccb196bf424f4a2963365f
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2025-01-09 18:01:00 -0300

    created DB and Swagger configs, and excecution of both on app.module and main

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,16 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function setupSwagger(app: INestApplication) {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('The-Fancy-Box-DB')
+    .setDescription(
+      'Base de datos para el proyecto final del grupo 1 de Henry - The Fancy Box',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('doc', app, document);
+}
