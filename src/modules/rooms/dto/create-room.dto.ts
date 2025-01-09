76a49e7e8c2258f184afc65ea526fe204b280430
pablomos <mosconpablo@gmail.com>
2025-01-09 12:15:51 -0300

    feat/create-rooms-dtos

--- src/modules/rooms/dto/create-room.dto.ts
+++ src/modules/rooms/dto/create-room.dto.ts
@@ -4,14 +4,17 @@ import { IsString, IsNumber, IsBoolean, IsArray } from 'class-validator';
 export class CreateRoomDto {
     @ApiProperty({
         description: 'Name of the room',
-        example: 'Deluxe Suite'
+        example: 'Deluxe Suite',
     })
     @IsString()
     name: string;
 
     @ApiProperty({
         description: 'Images URLs for the room',
-        example: ['https://example.com/product-image.jpg', 'https://example.com/product-image2.jpg'],
+        example: [
+            'https://example.com/product-image.jpg',
+            'https://example.com/product-image2.jpg',
+        ],
     })
     @IsArray()
     @IsString({ each: true })
@@ -19,22 +22,15 @@ export class CreateRoomDto {
 
     @ApiProperty({
         description: 'Description of the room',
-        example: 'A luxurious suite with a sea view.'
+        example: 'A luxurious suite with a sea view.',
     })
     @IsString()
     description: string;
 
     @ApiProperty({
         description: 'Price of the room',
-        example: 150.00
+        example: 150.0,
     })
     @IsNumber()
     price: number;
-
-    @ApiProperty({
-        description: 'Availability of the room',
-        example: true
-    })
-    @IsBoolean()
-    available: boolean;
 }
