30c2269b2b726c5c50bf8292315124c30acac75f
Eze <167648978+EzequielBonilla@users.noreply.github.com>
2025-01-10 13:46:10 -0300

    Update title & description swagger

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -3,9 +3,9 @@ import { INestApplication } from '@nestjs/common';
 
 export function setupSwagger(app: INestApplication) {
   const swaggerConfig = new DocumentBuilder()
-    .setTitle('The-Fancy-Box-DB')
+    .setTitle('The-Fancy-Box-API')
     .setDescription(
-      'Base de datos para el proyecto final del grupo 1 de Henry - The Fancy Box',
+      'API que concentra las funcionalidades necesarias para el proyecto final del grupo 1 de Henry - The Fancy Box',
     )
     .setVersion('1.0')
     .addBearerAuth()
