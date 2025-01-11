d79b007977a9b487780d7864fd56f4281f76cab6
pablomos <mosconpablo@gmail.com>
2025-01-10 21:29:58 -0300

    feat create rooms controller

--- src/modules/rooms/rooms.controller.ts
+++ src/modules/rooms/rooms.controller.ts
@@ -1,34 +1,85 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  NotFoundException,
+  BadRequestException
+} from '@nestjs/common';
 import { RoomsService } from './rooms.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Room } from './entities/room.entity';
 
+
+@ApiTags('Rooms')
 @Controller('rooms')
 export class RoomsController {
-  constructor(private readonly roomsService: RoomsService) {}
+  constructor(private readonly roomsService: RoomsService) { }
 
   @Post()
-  create(@Body() createRoomDto: CreateRoomDto) {
-    return this.roomsService.create(createRoomDto);
+  async create(@Body() createRoomDto: CreateRoomDto) {
+    return await this.roomsService.create(createRoomDto);
   }
 
   @Get()
-  findAll() {
-    return this.roomsService.findAll();
+  async findAll(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '5',
+  ) {
+    const pageNumber = Number(page);
+    const limitNumber = Number(limit);
+
+    if (isNaN(pageNumber) || pageNumber <= 0) {
+      throw new BadRequestException('Invalid page number');
+    }
+
+    if (isNaN(limitNumber) || limitNumber <= 0) {
+      throw new BadRequestException('Invalid limit number');
+    }
+    return await this.roomsService.findAll(pageNumber, limitNumber);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roomsService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const room = await this.roomsService.findOne(id);
+    if (!room || room.deleted_at) {
+      throw new NotFoundException(`Room with ID ${id} not found or has been deleted`);
+    }
+    return room;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
-    return this.roomsService.update(+id, updateRoomDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateRoomDto: UpdateRoomDto,
+  ) {
+    return await this.roomsService.update(id, updateRoomDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roomsService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.roomsService.remove(id);
   }
+
+  @Get('available')
+  async findAvailableRooms(
+    @Query('checkInDate') checkInDate: string,
+    @Query('checkOutDate') checkOutDate: string,
+  ): Promise<Room[]> {
+
+    const checkIn = new Date(checkInDate);
+    const checkOut = new Date(checkOutDate);
+
+    if (isNaN(checkIn.getTime()) || isNaN(checkOut.getTime())) {
+      throw new BadRequestException('Invalid check-in or check-out date');
+    }
+    return await this.roomsService.findAvailableRooms(checkIn, checkOut);
+  }
+
 }
