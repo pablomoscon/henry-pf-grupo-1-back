ee2b2177bd7d2048fa0eb022873736ccb23145f0
pablomos <mosconpablo@gmail.com>
2025-01-12 23:33:24 -0300

    feat/create login

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInAuthDto, } from './dto/signin-auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async authLogin(@Body() signInAuthDto: SignInAuthDto) {
+      const user = await this.authService.signIn(signInAuthDto);
+    return {
+      message: 'Login successful'
+      };
+  }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) { }
+
+  async findByEmailWithCredentials(email: string): Promise<User | null> {
+    return this.userRepository.findOne({
+      where: { email },
+      relations: ['credential'],
+    });
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
