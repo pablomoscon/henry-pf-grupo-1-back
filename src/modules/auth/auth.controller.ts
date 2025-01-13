7c768971fa7f16c3c8f48e5c8fbd7c70b2019e6a
Pablo Moscon <mosconpablo@gmail.com>
2025-01-13 06:39:57 -0300

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInAuthDto, } from './dto/signin-auth.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
