c941219e4a3b80e52e8e1fa7bc1b13c8496355e3
Eze <167648978+EzequielBonilla@users.noreply.github.com>
2025-01-14 22:43:56 -0300

    fix typo
    decia credentials y es credential, en singular

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -33,7 +33,7 @@ export class UsersService {
       where: { deleted_at: IsNull() },
       skip: (pageNumber - 1) * limitNumber,
       take: limitNumber,
-      relations: ['reservations', 'cats', 'credentials'],
+      relations: ['reservations', 'cats', 'credential'],
     });
   };
 
