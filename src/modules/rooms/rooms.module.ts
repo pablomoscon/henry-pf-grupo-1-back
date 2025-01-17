9ab1f0ff67ce0e98d149b047aa16ad39821eaa41
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2025-01-17 12:19:13 -0300

    added logic rooms with uploaded image

--- src/modules/rooms/rooms.module.ts
+++ src/modules/rooms/rooms.module.ts
@@ -3,11 +3,10 @@ import { RoomsService } from './rooms.service';
 import { RoomsController } from './rooms.controller';
 import { Room } from './entities/room.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FileUploadModule } from '../file-upload/file-upload.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Room])
-  ],
+  imports: [TypeOrmModule.forFeature([Room]), FileUploadModule],
   controllers: [RoomsController],
   providers: [RoomsService],
 })
--- src/modules/rooms/rooms.service.ts
+++ src/modules/rooms/rooms.service.ts
@@ -2,18 +2,45 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 import { Room } from './entities/room.entity';
-import { Between, IsNull, LessThanOrEqual, MoreThanOrEqual, Repository } from 'typeorm';
+import {
+  Between,
+  IsNull,
+  LessThanOrEqual,
+  MoreThanOrEqual,
+  Repository,
+} from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { FileUploadService } from 'src/modules/file-upload/file-upload.service';
 
 @Injectable()
 export class RoomsService {
   constructor(
     @InjectRepository(Room)
     private readonly roomsRepository: Repository<Room>,
+    private readonly fileUploadService: FileUploadService,
   ) {}
 
-  async create(createRoomDto: CreateRoomDto): Promise<Room> {
-    const newRoom = await this.roomsRepository.create(createRoomDto);
+  async create(
+    createRoomDto: CreateRoomDto,
+    file: Express.Multer.File,
+  ): Promise<Room> {
+    let imgUrl: string | undefined;
+
+    if (file) {
+      const uploadedFile = await this.fileUploadService.uploadFile({
+        fieldName: file.fieldname,
+        buffer: file.buffer,
+        originalName: file.originalname,
+        mimeType: file.mimetype,
+        size: file.size,
+      });
+      imgUrl = uploadedFile;
+    }
+
+    const newRoom = this.roomsRepository.create({
+      ...createRoomDto,
+      img: imgUrl,
+    });
     return this.roomsRepository.save(newRoom);
   }
 
@@ -36,8 +63,14 @@ export class RoomsService {
   ): Promise<Room[]> {
     const { checkInDate, checkOutDate, numberOfCats, priceRange } = filters;
 
-    const minPrice = priceRange?.minPrice && !isNaN(priceRange.minPrice) ? priceRange.minPrice : undefined;
-    const maxPrice = priceRange?.maxPrice && !isNaN(priceRange.maxPrice) ? priceRange.maxPrice : undefined;
+    const minPrice =
+      priceRange?.minPrice && !isNaN(priceRange.minPrice)
+        ? priceRange.minPrice
+        : undefined;
+    const maxPrice =
+      priceRange?.maxPrice && !isNaN(priceRange.maxPrice)
+        ? priceRange.maxPrice
+        : undefined;
 
     const rooms = await this.roomsRepository.find({
       relations: ['reservations'],
@@ -46,7 +79,8 @@ export class RoomsService {
         ...(numberOfCats !== undefined && { number_of_cats: numberOfCats }),
         ...(minPrice !== undefined && { price: MoreThanOrEqual(minPrice) }),
         ...(maxPrice !== undefined && { price: LessThanOrEqual(maxPrice) }),
-        ...(minPrice !== undefined && maxPrice !== undefined && { price: Between(minPrice, maxPrice) }),
+        ...(minPrice !== undefined &&
+          maxPrice !== undefined && { price: Between(minPrice, maxPrice) }),
       },
     });
 
@@ -54,18 +88,18 @@ export class RoomsService {
       return rooms.filter((room) =>
         room.reservations.every(
           ({ initial_date, ending_date }) =>
-            new Date(ending_date) < checkInDate || new Date(initial_date) > checkOutDate,
+            new Date(ending_date) < checkInDate ||
+            new Date(initial_date) > checkOutDate,
         ),
       );
     }
 
     return rooms;
-  };
-  
+  }
 
   async findOne(id: string) {
     return await this.roomsRepository.findOneBy({ id });
-  };
+  }
 
   async update(id: string, updateRoomDto: UpdateRoomDto) {
     const updateResult = await this.roomsRepository.update(id, updateRoomDto);
@@ -74,7 +108,7 @@ export class RoomsService {
       throw new NotFoundException(`Room with ID ${id} not found`);
     }
     return await this.findOne(id);
-  };
+  }
 
   async remove(id: string): Promise<Room> {
     const room = await this.roomsRepository.findOne({ where: { id } });
@@ -84,5 +118,5 @@ export class RoomsService {
     }
     room.deleted_at = new Date();
     return this.roomsRepository.save(room);
-  };
+  }
 }
