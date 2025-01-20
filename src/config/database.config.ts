72602d25ade7d87831dcf34f588e213f332b4def
Ezequiel Bonilla <ezequiel.bonilla92@gmail.com>
2025-01-20 13:58:19 -0300

    added type of connection to variables and respective ifs

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,22 +6,29 @@ dotenv.config({
   path: '.env.development.local',
 });
 
+const isLocal = process.env.POSTGRES_CONNECTION === 'local';
+
 const PostgresDatabaseOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT, 10),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  host: isLocal ? process.env.POSTGRES_HOST : process.env.POSTGRES_DEPLOY_HOST,
+  port: parseInt(
+    isLocal ? process.env.POSTGRES_PORT : process.env.POSTGRES_DEPLOY_PORT,
+    10,
+  ),
+  username: isLocal
+    ? process.env.POSTGRES_USER
+    : process.env.POSTGRES_DEPLOY_USER,
+  password: isLocal
+    ? process.env.POSTGRES_PASSWORD
+    : process.env.POSTGRES_DEPLOY_PASSWORD,
+  database: isLocal ? process.env.POSTGRES_DB : process.env.POSTGRES_DEPLOY_DB,
   synchronize: true,
   dropSchema: true,
   logging: false,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../**/migrations/*{.ts,.js}'],
   subscribers: [],
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: isLocal ? false : { rejectUnauthorized: false },
 };
 
 export const databaseConfig = registerAs(
