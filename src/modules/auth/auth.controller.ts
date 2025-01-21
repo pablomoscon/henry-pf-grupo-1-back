c63cc189e6e64cc0d44d4497f96ba0a4d209ec20
pablomos <mosconpablo@gmail.com>
2025-01-21 16:33:38 -0300

    fix/auth.controller -google oauth2

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import { SignupAuthDto } from './dto/signup-auth.dto';
 import { Response } from 'express';
 import { oauth2Client } from 'src/config/google-auth.config';
 import { AuthResponseDto } from './dto/response-auth.dto';
+import { ResponseUserDto } from '../users/dto/response-user.dto';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -42,14 +43,21 @@ export class AuthController {
     });
 
     res.json({ url: authUrl });
-  }
+  };
 
   @Get('google/callback')
   async handleGoogleCallback(@Query('code') code: string, @Res() res: Response) {
-    const token = await this.authService.googleSignUp(code);
+    const { token, user } = await this.authService.googleSignUp(code);
+    console.log('User:', user);
+    console.log('Token:', token);
 
-    res.setHeader('Authorization', `Bearer ${token}`);
-    res.redirect('http://localhost:3001/dashbord');
+    res.cookie(
+      'auth',
+      JSON.stringify({ token, user: new ResponseUserDto(user) }),
+      { httpOnly: true, secure: false }
+    );
+
+    res.redirect('http://localhost:3001/');
   };
 }
 
