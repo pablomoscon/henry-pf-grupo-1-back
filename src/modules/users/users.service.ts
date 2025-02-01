754744226a8654648c86aee854720ead0ab29c62
Pablo Moscon <mosconpablo@gmail.com>
2025-02-01 11:18:09 -0300

    Update users.service.ts

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -106,7 +106,7 @@ export class UsersService {
       where: { role: Role.USER, deleted_at: IsNull() },
       skip: (pageNumber - 1) * limitNumber,
       take: limitNumber,
-      relations: ['reservations', 'cats', 'messages', 'reviews'],
+      relations: ['reservations', 'cats', 'reviews', 'sentMessages', 'receivedMessages'],
     });
   };
 }
