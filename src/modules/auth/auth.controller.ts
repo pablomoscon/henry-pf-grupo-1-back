60f6eacd941ada10a043bd423e0a72f49a9172ef
pablomos <mosconpablo@gmail.com>
2025-02-07 08:24:29 -0300

    auth.controller2

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -81,7 +81,7 @@ export class AuthController {
       console.log('Generated token and user:', { token, user });
 
       res.cookie('auth', JSON.stringify({ token, user }), {
-        httpOnly: false,
+        httpOnly: true,
         secure: true,
         maxAge: 60 * 60 * 1000,
         sameSite: 'none'
